1ada1029203f1db483938594302d1be0190f91a9
leo <lzdragonix@gmail.com>
2015-07-04 19:16:21 +0800

    add baidu map sdk

--- app/src/main/java/com/scxrh/amb/view/fragment/LiveFragment.java
+++ app/src/main/java/com/scxrh/amb/view/fragment/LiveFragment.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.baidu.mapapi.SDKInitializer;
 import com.scxrh.amb.R;
 
 // 生活服务
@@ -14,6 +15,7 @@ public class LiveFragment extends BaseFragment
     @Override
     protected View genView(LayoutInflater inflater, ViewGroup container)
     {
+        SDKInitializer.initialize(getActivity().getApplicationContext());
         return inflater.inflate(R.layout.fragment_live, container, false);
     }
 }
