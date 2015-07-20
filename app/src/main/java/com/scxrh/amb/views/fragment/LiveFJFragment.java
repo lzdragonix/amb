ac1374804354dc721316e8b56078e52fdcfb0273
leo <lzdragonix@gmail.com>
2015-07-20 21:11:10 +0800

    show product detail

--- app/src/main/java/com/scxrh/amb/views/fragment/LiveFJFragment.java
+++ app/src/main/java/com/scxrh/amb/views/fragment/LiveFJFragment.java
@@ -1,22 +1,48 @@
 package com.scxrh.amb.views.fragment;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.scxrh.amb.R;
+
+import butterknife.Bind;
 
 // 生活 附近
 public class LiveFJFragment extends BaseFragment
 {
     public static final String TAG = LiveFJFragment.class.getSimpleName();
+    @Bind(R.id.mapView)
+    MapView mapView;
 
     @Override
-    protected int getLayoutId()
+    public void onResume()
     {
-        return R.layout.fragment_live_fj;
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 
     @Override
     protected void beforeContentView()
     {
         SDKInitializer.initialize(getActivity().getApplicationContext());
+    }
+
+    @Override
+    protected int getLayoutId()
+    {
+        return R.layout.fragment_live_fj;
     }
 }
