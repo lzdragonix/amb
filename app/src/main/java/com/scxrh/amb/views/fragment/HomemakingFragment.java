59fe8d300aad0b386487580bbbf5e2effbdd5834
leo <lzdragonix@gmail.com>
2015-08-16 10:46:53 +0800

    add homemaking

--- app/src/main/java/com/scxrh/amb/views/fragment/HomemakingFragment.java
+++ app/src/main/java/com/scxrh/amb/views/fragment/HomemakingFragment.java
@@ -1,18 +1,29 @@
 package com.scxrh.amb.views.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.scxrh.amb.App;
 import com.scxrh.amb.R;
-import com.scxrh.amb.common.Logger;
 import com.scxrh.amb.injector.component.DaggerMvpComponent;
 import com.scxrh.amb.injector.module.ActivityModule;
 import com.scxrh.amb.injector.module.MvpModule;
+import com.scxrh.amb.model.Homemaking;
 import com.scxrh.amb.presenter.HomemakingPresenter;
 import com.scxrh.amb.views.view.ProgressView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 // 家政服务
@@ -21,6 +32,10 @@ public class HomemakingFragment extends BaseFragment implements ProgressView
     public static final String TAG = HomemakingFragment.class.getSimpleName();
     @Inject
     HomemakingPresenter presenter;
+    @Bind(R.id.rvList)
+    RecyclerView mRecyclerView;
+    @Bind(R.id.txtHeader)
+    TextView txtHeader;
 
     @Override
     protected int getLayoutId()
@@ -43,6 +58,8 @@ public class HomemakingFragment extends BaseFragment implements ProgressView
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
+        txtHeader.setText("家政服务");
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         presenter.loadData();
     }
 
@@ -64,15 +81,65 @@ public class HomemakingFragment extends BaseFragment implements ProgressView
         toast(msg);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void showData(Object data)
     {
-        Logger.i(TAG, data.toString());
+        mRecyclerView.setAdapter(new RecyclerViewAdapter((List<Homemaking>)data));
     }
 
     @Override
     public void finish()
     {
         closeProgressDialog();
+    }
+
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+    {
+        private List<Homemaking> data;
+
+        public RecyclerViewAdapter(List<Homemaking> data)
+        {
+            this.data = data == null ? new ArrayList<>() : data;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.layout_homemaking_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            ViewHolder vh = (ViewHolder)holder;
+            Homemaking ho = data.get(position);
+            vh.name.setText(ho.getName());
+            vh.telephone.setText(ho.getTelephone());
+            vh.address.setText(ho.getAddress());
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return data.size();
+        }
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+        @Bind(R.id.name)
+        TextView name;
+        @Bind(R.id.telephone)
+        TextView telephone;
+        @Bind(R.id.address)
+        TextView address;
+
+        public ViewHolder(View view)
+        {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
